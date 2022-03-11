957f73ac5e4345e8662331f2cd20290d659f0f90
Salil Bagga <gaganmeet@hybrowlabs.com>
2022-03-11 23:07:43 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <h1 className="flex items-center justify-center text-3xl h-screen">
+        React,Tailwind And Typescript by Salkoro
+      </h1>
+    </div>
+  );
+};
+
+export default App;
